ce94045ebd81e76b9426e027a9067d7fc8d20d4f
[peyton moore] <[sinclair8866@gmail.com]>
2023-12-31 21:22:53 -0600

    Draw-as-many-cats-as-you-want

---
+++ Draw-as-many-cats-as-you-want.js
@@ -0,0 +1,10 @@
+ // Draw as many cats as you want!
+ var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log("Cat Drawings", i + " =^.^=");
+    }
+};
+drawCats(4); //You can put any number here instead of 22
+// Creating a variable
+var age = 12;
+console.log("Age", age);
